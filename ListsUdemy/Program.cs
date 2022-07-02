db983288c04bdc82799642ddaeaef74502471c80
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-02 06:31:00 +0200

    Add project files.

---
+++ ListsUdemy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating lists:
+            var numbers = new List<int>();
+
+            var numbers2 = new List<int>() { 1,2,3,4};
+            
+            // useful methods:
+            /* 
+             * Add()
+             * AddRange()
+             * Remove()
+             * RemoveAt()
+             * IndexOf()
+             * Contains()
+             * Count
+             */
+        }
+    }
+}
