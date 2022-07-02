d1ae7826214f4a8401dd5f59df55ce10bad7edc1
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-02 06:53:54 +0200

    link

--- ListsUdemy/Program.cs
+++ ListsUdemy/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//https://www.udemy.com/course/csharp-tutorial-for-beginners/learn/lecture/2979536#content
 namespace ListsUdemy
 {
     class Program
